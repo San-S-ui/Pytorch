703193a3aa7e1cb290e737656706c026be6321cd
“San-S-ui” <zymg05@outlook.com>
2026-07-17 23:48:08 +0800

    练习

---
+++ pytorch/张量和numpy相互转换.py
@@ -0,0 +1,49 @@
+'''
+张量和numpy相互转换
+.detach().numpy()方法：张量转numpy 共享内存
+detach()复制张量，返回一个新的张量，和原来的张量共享内存
+
+.numpy().copy()方法：张量转numpy 不共享内存
+
+from_numpy()方法：numpy转张量 共享内存
+torch.tensor()方法：numpy转张量 不共享内存
+'''
+import torch
+import numpy as np
+# def tensor_numpy():
+#     # 张量转numpy
+#     t1 =torch.tensor([1,2,3,4,5])
+#     n1 = t1.numpy()
+#     print(t1,type(t1))
+#     print(n1,type(n1))
+#     print('-'*50)
+#     #测试共享内存
+#     n1[0]=100
+#     print(t1,type(t1))
+#     print(n1,type(n1))
+def tensor_numpy1():
+    # numpy转张量
+    n1 =np.array([5,4,3,2,1])
+    t1= torch.from_numpy(n1)
+    print(t1,type(t1))
+    print(n1,type(n1))
+    print('-'*50)
+    #测试共享内存
+    t3 = torch.tensor(n1) 
+    n1[0]=100
+    print(t1,type(t1))
+    print(n1,type(n1))
+    print(t3,type(t3))
+def demo():
+    #从标量张量中提取值
+    t1 = torch.tensor(100)
+    a = t1.item()
+    print(t1,type(t1))
+    print(value,type(a))
+if __name__=='__main__':
+    # tensor_numpy()
+    # tensor_numpy1()
+    demo()
+
+
+    
---
+++ pytorch/张量的索引.py
@@ -0,0 +1,21 @@
+import torch
+torch.manual_seed(42)
+t1 = torch.randint(1,10,(5,5))
+print(t1)
+#简单行列
+print(t1[1])
+print(t1[1,:2]) #第一行前两列
+print(t1[:,2])#所有行第二列
+print('-'*30)
+#列表索引
+print(t1[[2,1],[3,4]]) #返回[2，3]和[1，4]的内容
+#范围索引
+print(t1[:3,:2]) #前3行前2列
+print(t1[1::2,::2])#奇数行,偶数列
+#布尔索引
+print(t1[torch.tensor([True,False,False,False,True]),:])#第0行和最后一行
+print(t1[t1[:,2]>5])#第2列大于5的行数据
+print(t1[:,t1[2]>5])#第2行大于5的列数据
+print(t1[2,t1[2]>5])
+#多维索引
+s
